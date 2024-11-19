6742d7dcd37385cea2d718f1ec16623a66765a39
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-11-19 12:31:23 +0000

    chore: use biome recommended lints

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// biome-ignore lint: lib requires default export
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
