f9b41483c68e5611c6210d3a81519c3e938251b9
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-11-25 14:07:37 +0000

    feat: limit data upload to `10_000` records in one request

--- src/handlers/data-handle-upload.ts
+++ src/handlers/data-handle-upload.ts
@@ -6,9 +6,11 @@ import { UUID } from "mongodb";
 import { z } from "zod";
 import type { AppEnv } from "#/app";
 import { type Handler, foldToApiResponse } from "#/handlers/handler";
-import { DataRepository } from "#/models/data";
+import { DataRepository, type InsertResult } from "#/models/data";
 import { SchemasRepository } from "#/models/schemas";
 import { Uuid, type UuidDto } from "#/types";
+
+export const MAX_RECORDS_LENGTH = 10_000;
 
 export const UploadDataRequestBody = z.object({
   schema: Uuid,
@@ -22,8 +24,7 @@ export type UploadDataRequestBody = {
 export type UploadDataHandler = Handler<{
   path: "/api/v1/data";
   request: UploadDataRequestBody;
-  // TODO: break down to updated / inserted ... could also return uuids?
-  response: number;
+  response: InsertResult;
 }>;
 
 export function dataHandleUpload(
@@ -39,25 +40,33 @@ export function dataHandleUpload(
         return result.success ? E.succeed(result.data) : E.fail(result.error);
       }),
 
+      E.flatMap((request) => {
+        return request.data.length <= MAX_RECORDS_LENGTH
+          ? E.succeed(request)
+          : E.fail(
+              new Error(`Max data length is ${MAX_RECORDS_LENGTH} elements`),
+            );
+      }),
+
       E.flatMap((request) =>
         pipe(
           E.Do,
-          E.bind("document", () =>
+          E.bind("schema", () =>
             SchemasRepository.find(new UUID(request.schema)),
           ),
-          E.bind("data", ({ document }) => {
+          E.bind("data", ({ schema }) => {
             const ajv = new Ajv({ strict: "log" });
             addFormats(ajv);
 
-            const validator = ajv.compile(document.schema);
+            const validator = ajv.compile(schema.schema);
             const valid = validator(request.data);
 
             return valid
               ? E.succeed(request.data)
               : E.fail(new ValidationError(validator.errors ?? []));
           }),
-          E.flatMap(({ document, data }) => {
-            return DataRepository.insert(c.var.db.data, document._id, data);
+          E.flatMap(({ schema, data }) => {
+            return DataRepository.insert(c.var.db.data, schema, data);
           }),
         ),
       ),
