3959ab4eeb3c578f60de513c3ed9d60a99b42d60
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-12-03 14:08:28 +0000

    feat: add prometheus `/metrics` endpoint; reduce log noise; enable http compression

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,8 +1,30 @@
-import pinoHttp from "pino-http";
-import { createLogger } from "#/common/logging";
+import pino, { type Logger } from "pino";
+import pinoHttp, { type HttpLogger } from "pino-http";
 import type { Config } from "#/env";
 
-export function loggerMiddleware(env: Config) {
-  const logger = createLogger(env);
-  return pinoHttp({ logger });
+export function loggerMiddleware(env: Config): HttpLogger {
+  return pinoHttp({
+    logger: createLogger(env),
+    customLogLevel: (_req, res, err) => {
+      const code = res.statusCode;
+      if (400 <= code && code < 500) {
+        return "warn";
+      }
+
+      if (500 <= code || err) {
+        return "error";
+      }
+
+      if (300 <= code && code < 400) {
+        return "silent";
+      }
+      return "debug";
+    },
+  });
+}
+
+export function createLogger(env: Config): Logger {
+  return pino({
+    level: env.logLevel,
+  });
 }
