a74034010d4e17dcbff7a035732f18530e75db0b
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-12-04 08:36:34 +0000

    feat: refine `/about` to include build info
    Example from `GET /about`:
    
    ```json
    {
    	"started": "2024-12-04T08:34:59.527Z",
    	"build": {
    		"time": "1970-01-01T00:00:00Z",
    		"commit": "unknown",
    		"version": "0.0.0"
    	},
    	"address": "nillion11q073p3a4wjhw3kffnev3c675mnf0j5rk86tr6s",
    	"publicKey": "A0wcqQiz1QW1jOiqXVgD8qilY2RB3IB6qTyMKiym0ATZ",
    	"endpoint": "http://localhost:8080"
    }
    ```

---
+++ src/system/service.ts
@@ -0,0 +1,53 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import type { Context } from "#/env";
+
+export type AboutNode = {
+  started: Date;
+  build: BuildInfo;
+  address: string;
+  publicKey: string;
+  endpoint: string;
+};
+
+type BuildInfo = {
+  time: string;
+  commit: string;
+  version: string;
+};
+
+const started = new Date();
+let buildInfo: BuildInfo;
+
+export function getNodeInfo(context: Context): AboutNode {
+  return {
+    started,
+    build: getBuildInfo(context),
+    address: context.node.address,
+    publicKey: context.node.publicKey,
+    endpoint: context.node.endpoint,
+  };
+}
+
+function getBuildInfo(context: Context): BuildInfo {
+  if (buildInfo) {
+    return buildInfo;
+  }
+
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const buildInfoPath = path.join(__dirname, "../../buildinfo.json");
+    const content = fs.readFileSync(buildInfoPath, "utf-8");
+    return JSON.parse(content) as BuildInfo;
+  } catch (error) {
+    context.log.info("No buildinfo.json found using fallback values");
+    buildInfo = {
+      time: "1970-01-01T00:00:00Z",
+      commit: "unknown",
+      version: "0.0.0",
+    };
+    return buildInfo;
+  }
+}
