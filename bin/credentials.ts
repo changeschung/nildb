55bd5896981d4ea2703e80c26eebb93d3a245dcc
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-12-16 18:58:04 +0000

    feat: add `credentials` script for generating a key pairs and jwts

---
+++ bin/credentials.ts
@@ -0,0 +1,74 @@
+import { Command } from "commander";
+import { z } from "zod";
+import { Identity } from "#/common/identity";
+import { PRIVATE_KEY_LENGTH, PUBLIC_KEY_LENGTH } from "../src/env";
+
+const Args = z.object({
+  secretKey: z.string().length(PRIVATE_KEY_LENGTH).optional(),
+  nodePublicKey: z.string().length(PUBLIC_KEY_LENGTH).optional(),
+  mainnet: z.boolean(),
+});
+type Args = z.infer<typeof Args>;
+
+type Output = {
+  client: {
+    sk: string;
+    pk: string;
+    did: string;
+    jwt?: string;
+  };
+  node?: {
+    pk: string;
+    did: string;
+  };
+};
+
+async function main(): Promise<void> {
+  const program = new Command();
+
+  program
+    .name("nilDB credential utils")
+    .description("Create credentials for nilDB communications")
+    .option("-s, --secret-key <key>", "The client's hex-encoded secret key")
+    .option(
+      "-n, --node-public-key <key>",
+      "The node's hex-encoded public key. Required for JWT generation.",
+    )
+    .option(
+      "-m --mainnet",
+      "Generate mainnet DIDs (defaults to testnet if not specified)",
+      false,
+    )
+    .version("0.0.1");
+
+  program.parse(process.argv);
+  const options = Args.parse(program.opts<Args>());
+  process.env.APP_ENV = options.mainnet ? "mainnet" : "testnet";
+
+  const result: Partial<Output> = {};
+
+  const identity = options.secretKey
+    ? Identity.fromSk(options.secretKey)
+    : Identity.new();
+  result.client = {
+    sk: identity.secretKey,
+    pk: identity.publicKey,
+    did: identity.did,
+  };
+
+  if (options.nodePublicKey) {
+    const nodePk = options.nodePublicKey;
+    const nodeDid = Identity.didFromPk(nodePk);
+    const jwt = await identity.createJwt({ aud: nodeDid });
+
+    result.client.jwt = jwt;
+    result.node = {
+      did: nodeDid,
+      pk: nodePk,
+    };
+  }
+
+  console.log(result);
+}
+
+await main();
