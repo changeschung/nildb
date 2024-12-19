28501477b943590ce160336aa2fc3c177e8475da
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-12-19 13:45:54 +0000

    feat(docs): update openapi spec to reflect latest public api endpoints

--- src/docs/routes.ts
+++ src/docs/routes.ts
@@ -3,7 +3,7 @@ import swaggerUi from "swagger-ui-express";
 import { serveApiDocsController } from "#/docs/controllers";
 
 export const ApiDocsEndpoint = {
-  Docs: "/api-docs",
+  Docs: "/api/v1/openapi/docs",
 } as const;
 
 export function buildApiDocsRoutes() {
