5ed11bf99159d768c3f9f57ff223cc21b415a914
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2025-01-14 11:35:31 +0000

    feat(admin): add data operation endpoints

--- src/admin/controllers.ts
+++ src/admin/controllers.ts
@@ -1,11 +1,16 @@
 import { Effect as E, pipe } from "effect";
 import type { RequestHandler } from "express";
-import type { EmptyObject } from "type-fest";
+import type { EmptyObject, JsonArray } from "type-fest";
 import { z } from "zod";
+import type { OrganizationAccountDocument } from "#/accounts/repository";
 import { type ApiResponse, foldToApiResponse } from "#/common/handler";
+import type { DocumentBase } from "#/common/mongo";
 import { NilDid } from "#/common/nil-did";
-import type { UuidDto } from "#/common/types";
+import { Uuid, type UuidDto } from "#/common/types";
 import { parseUserData } from "#/common/zod-utils";
+import { MAX_RECORDS_LENGTH } from "#/data/controllers";
+import type { CreatedResult, UpdateResult } from "#/data/repository";
+import { DataService } from "#/data/service";
 import { PUBLIC_KEY_LENGTH } from "#/env";
 import { isRoleAllowed } from "#/middleware/auth";
 import type { AccountDocument } from "./repository";
@@ -88,8 +93,197 @@ const removeAccount: RequestHandler<
   );
 };
 
+export const DeleteDataRequest = z.object({
+  schema: Uuid,
+  filter: z
+    .record(z.string(), z.unknown())
+    .refine((obj) => Object.keys(obj).length > 0, {
+      message: "Filter cannot be empty",
+    }),
+});
+export type DeleteDataRequest = z.infer<typeof DeleteDataRequest>;
+export type DeleteDataResponse = ApiResponse<number>;
+
+const deleteData: RequestHandler<
+  EmptyObject,
+  DeleteDataResponse,
+  DeleteDataRequest
+> = async (req, res) => {
+  const { ctx, body } = req;
+
+  if (!isRoleAllowed(req, ["admin"])) {
+    res.sendStatus(401);
+    return;
+  }
+
+  await pipe(
+    parseUserData<DeleteDataRequest>(() => DeleteDataRequest.parse(body)),
+    E.flatMap((payload) => DataService.deleteRecords(ctx, payload)),
+    foldToApiResponse(req, res),
+    E.runPromise,
+  );
+};
+
+export const FlushDataRequest = z.object({
+  schema: Uuid,
+});
+export type FlushDataRequest = z.infer<typeof FlushDataRequest>;
+export type FlushDataResponse = ApiResponse<number>;
+
+const flushData: RequestHandler<
+  EmptyObject,
+  FlushDataResponse,
+  FlushDataRequest
+> = async (req, res) => {
+  const { ctx, body } = req;
+
+  if (!isRoleAllowed(req, ["admin"])) {
+    res.sendStatus(401);
+    return;
+  }
+
+  await pipe(
+    parseUserData<FlushDataRequest>(() => TailDataRequest.parse(body)),
+    E.flatMap((payload) => DataService.flushCollection(ctx, payload.schema)),
+    foldToApiResponse(req, res),
+    E.runPromise,
+  );
+};
+
+export const ReadDataRequest = z.object({
+  schema: Uuid,
+  filter: z.record(z.string(), z.unknown()),
+});
+export type ReadDataRequest = z.infer<typeof ReadDataRequest>;
+export type ReadDataResponse = ApiResponse<DocumentBase[]>;
+
+const readData: RequestHandler<
+  EmptyObject,
+  ReadDataResponse,
+  ReadDataRequest
+> = async (req, res) => {
+  const { ctx, body } = req;
+  if (!isRoleAllowed(req, ["admin"])) {
+    res.sendStatus(401);
+    return;
+  }
+
+  await pipe(
+    parseUserData<ReadDataRequest>(() => ReadDataRequest.parse(body)),
+    E.flatMap((payload) => DataService.readRecords(ctx, payload)),
+    foldToApiResponse(req, res),
+    E.runPromise,
+  );
+};
+
+export const TailDataRequest = z.object({
+  schema: Uuid,
+});
+export type TailDataRequest = z.infer<typeof TailDataRequest>;
+export type TailDataResponse = ApiResponse<JsonArray>;
+
+const tailData: RequestHandler<
+  EmptyObject,
+  TailDataResponse,
+  TailDataRequest
+> = async (req, res) => {
+  const { ctx, body } = req;
+
+  if (!isRoleAllowed(req, ["admin"])) {
+    res.sendStatus(401);
+    return;
+  }
+
+  await pipe(
+    parseUserData<TailDataRequest>(() => TailDataRequest.parse(body)),
+    E.flatMap((payload) => DataService.tailData(ctx, payload.schema)),
+    foldToApiResponse(req, res),
+    E.runPromise,
+  );
+};
+
+export const UploadDataRequest = z.object({
+  schema: Uuid,
+  data: z
+    .array(z.record(z.string(), z.unknown()))
+    .refine(
+      (elements) =>
+        elements.length > 0 && elements.length <= MAX_RECORDS_LENGTH,
+      { message: `Length must be non zero and lte ${MAX_RECORDS_LENGTH}` },
+    ),
+});
+export type UploadDataRequest = z.infer<typeof UploadDataRequest>;
+export type PartialDataDocumentDto = UploadDataRequest["data"] & {
+  _id: UuidDto;
+};
+export type UploadDataResponse = ApiResponse<CreatedResult>;
+
+const uploadData: RequestHandler<
+  EmptyObject,
+  UploadDataResponse,
+  UploadDataRequest
+> = async (req, res) => {
+  const { ctx, body } = req;
+
+  if (!isRoleAllowed(req, ["admin"])) {
+    res.sendStatus(401);
+    return;
+  }
+  const account = req.account as OrganizationAccountDocument;
+
+  await pipe(
+    parseUserData<UploadDataRequest>(() => UploadDataRequest.parse(body)),
+    E.flatMap((payload) => {
+      return DataService.createRecords(
+        ctx,
+        account._id,
+        payload.schema,
+        payload.data,
+      );
+    }),
+    foldToApiResponse(req, res),
+    E.runPromise,
+  );
+};
+
+export const UpdateDataRequest = z.object({
+  schema: Uuid,
+  filter: z.record(z.string(), z.unknown()),
+  update: z.record(z.string(), z.unknown()),
+});
+export type UpdateDataRequest = z.infer<typeof UpdateDataRequest>;
+export type UpdateDataResponse = ApiResponse<UpdateResult>;
+
+const updateData: RequestHandler<
+  EmptyObject,
+  UpdateDataResponse,
+  UpdateDataRequest
+> = async (req, res) => {
+  const { ctx, body } = req;
+
+  if (!isRoleAllowed(req, ["admin"])) {
+    res.sendStatus(401);
+    return;
+  }
+
+  await pipe(
+    parseUserData<UpdateDataRequest>(() => UpdateDataRequest.parse(body)),
+    E.flatMap((body) => {
+      return DataService.updateRecords(ctx, body);
+    }),
+    foldToApiResponse(req, res),
+    E.runPromise,
+  );
+};
+
 export const AdminController = {
   createAdminAccount,
   listAccounts,
   removeAccount,
+  deleteData,
+  flushData,
+  readData,
+  tailData,
+  uploadData,
+  updateData,
 };
--- src/admin/routes.ts
+++ src/admin/routes.ts
@@ -3,6 +3,14 @@ import { AdminController } from "./controllers";
 
 export const AdminEndpointV1 = {
   Accounts: "/api/v1/admin/accounts",
+  Data: {
+    Delete: "/api/v1/admin/data/delete",
+    Flush: "/api/v1/admin/data/flush",
+    Read: "/api/v1/admin/data/read",
+    Tail: "/api/v1/admin/data/tail",
+    Update: "/api/v1/admin/data/update",
+    Upload: "/api/v1/admin/data/upload",
+  },
 } as const;
 
 export function buildAdminRouter(): Router {
@@ -14,6 +22,13 @@ export function buildAdminRouter(): Router {
     `${AdminEndpointV1.Accounts}/:accountDid`,
     AdminController.removeAccount,
   );
+
+  router.post(AdminEndpointV1.Data.Delete, AdminController.deleteData);
+  router.post(AdminEndpointV1.Data.Flush, AdminController.flushData);
+  router.post(AdminEndpointV1.Data.Read, AdminController.readData);
+  router.post(AdminEndpointV1.Data.Tail, AdminController.tailData);
+  router.post(AdminEndpointV1.Data.Update, AdminController.updateData);
+  router.post(AdminEndpointV1.Data.Upload, AdminController.uploadData);
 
   return router;
 }
