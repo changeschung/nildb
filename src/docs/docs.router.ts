19897061eb311a65ed3008405d5ebdc4b46753f8
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2025-02-10 15:49:39 +0000

    docs: add beta open api spec; move specs into files alongside each route
    The single openapi.yaml file was becoming unwieldy. This moves the openapi spec alongside each feature which will hopefully keep the files shorter, and as they're now closer to the endpoints they describe, easier to maintain.

--- src/docs/docs.router.ts
+++ src/docs/docs.router.ts
@@ -1,18 +1,27 @@
-import fs from "node:fs";
-import nodePath from "node:path";
+import path from "node:path";
 import { fileURLToPath } from "node:url";
+import refParser from "@apidevtools/json-schema-ref-parser";
 import { swaggerUI } from "@hono/swagger-ui";
-import yaml from "js-yaml";
 import type { App } from "#/app";
 import { PathsV1 } from "#/common/paths";
 import type { AppBindings } from "#/env";
 
-export function createOpenApiRouter(app: App, _bindings: AppBindings): void {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = nodePath.dirname(__filename);
-  const openApiPath = nodePath.join(__dirname, "./openapi.yaml");
-  const openApiYaml = fs.readFileSync(openApiPath, "utf8");
-  const spec = yaml.load(openApiYaml) as object;
+export function createOpenApiRouter(app: App, bindings: AppBindings): void {
+  const filename = fileURLToPath(import.meta.url);
+  const srcDocsDir = path.dirname(filename);
+  const openApiPath = path.join(srcDocsDir, "./base.openapi.yaml");
 
-  app.use(`${PathsV1.docs}/*`, swaggerUI({ url: "", spec }));
+  refParser
+    .bundle(openApiPath, {
+      dereference: {
+        circular: true,
+      },
+    })
+    .then((spec) => {
+      app.use(`${PathsV1.docs}/*`, swaggerUI({ url: "", spec }));
+      bindings.log.info("Openapi spec and endpoint ready");
+    })
+    .catch((error) => {
+      bindings.log.info("Failed to load openapi spec: %O", error);
+    });
 }
