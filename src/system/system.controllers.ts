8dad49f2e85dfd4e13c46a3bb0e71b0ea5969b09
Dev <devanandgowda8@gmail.com>
2025-02-12 01:05:25 +0530

    feat: update `healthCheck` controller to handle maintenance
    Respond with "Service Unavailable" when in maintenance, but with 200 status code

--- src/system/system.controllers.ts
+++ src/system/system.controllers.ts
@@ -1,3 +1,5 @@
+import { Effect as E, pipe } from "effect";
+import { StatusCodes, getReasonPhrase } from "http-status-codes";
 import type { App } from "#/app";
 import { SystemEndpoint } from "#/system/system.router";
 import * as SystemService from "./system.services";
@@ -10,5 +12,18 @@ export function aboutNode(app: App): void {
 }
 
 export function healthCheck(app: App): void {
-  app.get(SystemEndpoint.Health, (c) => c.text("OK"));
+  app.get(SystemEndpoint.Health, async (c) => {
+    return await pipe(
+      SystemService.getMaintenanceStatus(c.env),
+      E.flatMap((maintenanceStatus) => {
+        if (maintenanceStatus.active) {
+          return E.succeed(
+            c.text(getReasonPhrase(StatusCodes.SERVICE_UNAVAILABLE)),
+          );
+        }
+        return E.succeed(c.text(getReasonPhrase(StatusCodes.OK)));
+      }),
+      E.runPromise,
+    );
+  });
 }
